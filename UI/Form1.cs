e4a5b16531c08a5b822032585ad9b8fd87822852
NAIK <s-dhnaik@lwsd.org>
2020-02-26 10:27:06 -0800

    Add project files.

---
+++ UI/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Player Player1 = new Player();
+            label1.Text = Player1.DisplayPlayer();
+        }
+    }
+}
---
+++ UI/Ui.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    class Ui
+    {
+    
+
+        
+    }
+    public class Player {
+        public String DisplayPlayer(/*gameLocation, playerobject*/)
+        {
+            //displays the player on the screen in the given location
+            return "Player";
+        }
+        public int DisplayHS(/*HighScore, Score*/)
+        {
+            //when tab is hit a popup apear on screen displaying the current score and highScore
+            int HS = 1000;
+            return HS;
+        }
+        public void DisplayTrivia(/*TriviaQs*/)
+        {
+            //Displays the trivia questions
+        }
+        public void DisplayWumpus(/*Wumpus*/)
+        {
+            //When player encounters wumpus the wumpous is displayed on screen.
+        }
+    }
+
+
+}
